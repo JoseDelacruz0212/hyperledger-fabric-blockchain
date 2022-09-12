08799c1c0292b244c58eb6da7455759692c70392
José <zhaotian001@gmail.com>
2022-09-12 17:26:12 -0500

    rollback fabcar.go chaincode

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -8,12 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
-	"bytes"
-	"strings"
-	"time"
 
-	"github.com/hyperledger/fabric-chaincode-go/shim"
-	pb "github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
@@ -39,7 +34,9 @@ type QueryResult struct {
 // InitLedger adds a base set of cars to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	cars := []Car{
+		
 	}
+
 	for i, car := range cars {
 		carAsBytes, _ := json.Marshal(car)
 		err := ctx.GetStub().PutState(""+strconv.Itoa(i), carAsBytes)
@@ -128,71 +125,6 @@ func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterfa
 	carAsBytes, _ := json.Marshal(car)
 
 	return ctx.GetStub().PutState(carNumber, carAsBytes)
-}
-
-func (t *SmartContract) getHistoryForTransaction(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-
-	if len(args) < 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	carid := args[0]
-
-	fmt.Printf("- start getHistory: %s\n", carid)
-
-	resultsIterator, err := stub.GetHistoryForKey(carid)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	defer resultsIterator.Close()
-
-	// buffer is a JSON array containing historic values for the marble
-	var buffer bytes.Buffer
-	buffer.WriteString("[")
-
-	bArrayMemberAlreadyWritten := false
-	for resultsIterator.HasNext() {
-		response, err := resultsIterator.Next()
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-		// Add a comma before array members, suppress it for the first array member
-		if bArrayMemberAlreadyWritten == true {
-			buffer.WriteString(",")
-		}
-		buffer.WriteString("{\"TxId\":")
-		buffer.WriteString("\"")
-		buffer.WriteString(response.TxId)
-		buffer.WriteString("\"")
-
-		buffer.WriteString(", \"Value\":")
-		// if it was a delete operation on given key, then we need to set the
-		//corresponding value null. Else, we will write the response.Value
-		//as-is (as the Value itself a JSON marble)
-		if response.IsDelete {
-			buffer.WriteString("null")
-		} else {
-			buffer.WriteString(string(response.Value))
-		}
-
-		buffer.WriteString(", \"Timestamp\":")
-		buffer.WriteString("\"")
-		buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
-		buffer.WriteString("\"")
-
-		buffer.WriteString(", \"IsDelete\":")
-		buffer.WriteString("\"")
-		buffer.WriteString(strconv.FormatBool(response.IsDelete))
-		buffer.WriteString("\"")
-
-		buffer.WriteString("}")
-		bArrayMemberAlreadyWritten = true
-	}
-	buffer.WriteString("]")
-
-	fmt.Printf("- getHistoryForUser returning:\n%s\n", buffer.String())
-
-	return shim.Success(buffer.Bytes())
 }
 
 func main() {
