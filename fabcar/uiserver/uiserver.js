999d8349ce233492e6757c7274850e43dc79be80
José <zhaotian001@gmail.com>
2022-09-11 23:08:05 -0500

    pls funca

--- fabcar/uiserver/uiserver.js
+++ fabcar/uiserver/uiserver.js
@@ -105,6 +105,47 @@ app.get('/api/query/:user_id', async function (req, res) {
                 
         }
 });
+
+app.get('/api/history/:user_id', async function (req, res) {
+        try {
+                const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+                const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+                // Create a new file system based wallet for managing identities.
+                const walletPath = path.join(process.cwd(), 'wallet');
+                const wallet = await Wallets.newFileSystemWallet(walletPath);
+                console.log(`Wallet path: ${walletPath}`);
+
+                // Check to see if we've already enrolled the user.
+                const identity = await wallet.get('appUser1');
+                if (!identity) {
+                        console.log('An identity for the user "appUser1" does not exist in the wallet');
+                        console.log('Run the registerUser.js application before retrying');
+                        return;
+                }
+                // Create a new gateway for connecting to our peer node.
+                const gateway = new Gateway();
+                await gateway.connect(ccp, { wallet, identity: 'appUser1', discovery: { enabled: true, asLocalhost: true } });
+
+                // Get the network (channel) our contract is deployed to.
+                const network = await gateway.getNetwork('mychannel');
+
+                // Get the contract from the network.
+                const contract = network.getContract('fabcar');
+                // Evaluate the specified transaction.
+                // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+                // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+                const result = await contract.evaluateTransaction('getHistoryForTransaction', req.params.user_id);
+                console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+                res.status(200).json({ response:JSON.parse(result.toString()) });
+        } catch (error) {
+                console.error(`Failed to evaluate transaction: ${error}`);
+                res.status(500).json({ error: error });
+                
+        }
+});
+
+
+
 app.post('/api/addTransaction/', urlencodedParser, async function (req, res) {
         try {
 
